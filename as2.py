334671fbc766a2292dde7c95838a2d763a425d43
DrakeSeifert <drakeseifert1@gmail.com>
2017-02-21 15:17:29 -0800

    Fixed list splitting

--- as2.py
+++ as2.py
@@ -3,15 +3,24 @@
 cost = []
 with open('imp2cost.txt') as inputfile:
 	for line in inputfile:
-		cost.append(line.strip().split('\n'))
+		cost.append(line.strip().split(','))
 inputfile.close()
 
 inputChars = []
 with open('imp2input.txt') as inputfile:
 	for line in inputfile:
-		inputChars.append(line.strip().split('\n'))
+		inputChars.append(line.strip().split(','))
 inputfile.close()
 
+print len(cost[0])
+
+#omit the "*" character
+myChars = [];
+for i in range(1, len(cost[0])):
+	myChars.append(cost[0][i])
+
 print cost
+print "\n\n\n"
+print myChars
 print "\n\n\n"
 print inputChars
